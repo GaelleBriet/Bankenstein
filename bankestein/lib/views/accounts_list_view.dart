9ce2a5d2424045569d74018e11310614cbd76a10
GaelleBriet <gaelle.briet@protonmail.com>
2024-02-16 13:52:55 +0100

    added new page accounts list and route

---
+++ bankestein/lib/views/accounts_list_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AccountsListView extends StatelessWidget {
+  const AccountsListView({super.key});
+
+  static const String pageName = 'accounts';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
