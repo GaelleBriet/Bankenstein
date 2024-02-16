31e0dafde94adf57f76ec9510cc70077f989db9b
GaelleBriet <gaelle.briet@protonmail.com>
2024-02-16 13:35:56 +0100

    added logout btn

--- bankestein/lib/views/settings_view.dart
+++ bankestein/lib/views/settings_view.dart
@@ -1,3 +1,4 @@
+import 'package:bankestein/bloc/authentication_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,13 +17,38 @@ class SettingsView extends StatelessWidget {
     final settingsCubit = context.watch<SettingCubit>();
     return Scaffold(
       appBar: const NavigationBarTop(title: 'Settings', userName: userName),
-      body: Center(
-        child: CheckboxListTile(
-          title: const Text('Activate dark theme '),
-          value: settingsCubit.state.brightness == Brightness.dark,
-          onChanged: (bool? value) {
-            settingsCubit.toggleTheme();
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CheckboxListTile(
+                title: const Text('Activate dark theme '),
+                value: settingsCubit.state.brightness == Brightness.dark,
+                onChanged: (bool? value) {
+                  settingsCubit.toggleTheme();
+                },
+              ),
+              const Spacer(),
+              BlocBuilder<AuthenticationCubit, AuthenticationState>(
+                builder: (context, state) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      context.read<AuthenticationCubit>().logout();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme
+                          .of(context)
+                          .primaryColor,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Log Out'),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: const NavigationBarBottom(selectedIndex: 4),
