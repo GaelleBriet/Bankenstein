b17dde757199cfc1e760fc5ef0d71635da640bca
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-04 10:28:15 +0100

    added intl package, format date and add title in account detail view

--- bankestein/lib/views/account_view.dart
+++ bankestein/lib/views/account_view.dart
@@ -1,6 +1,7 @@
 import 'package:bankestein/bloc/authentication_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../bloc/account_cubit.dart';
 import '../widgets/Navigation_bar_bottom.dart';
@@ -31,17 +32,31 @@ class AccountView extends StatelessWidget {
           if (state is AccountLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is AccountLoaded) {
-            return ListView.builder(
-              itemCount: state.accounts[0].transactions?.length,
-              itemBuilder: (context, index) {
-                final transaction = state.accounts[0].transactions?[index];
-                final balanceInEuros = transaction!.amount / 100;
-                return ListTile(
-                  title: Text(transaction!.name),
-                  subtitle: Text(transaction!.date),
-                  trailing: Text("${balanceInEuros.toStringAsFixed(2)}€"),
-                );
-              },
+            return Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                  'Last transactions',
+                  style: TextStyle(fontSize: 24,
+                  fontWeight: FontWeight.w400),
+                  ),
+                ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: state.accounts[0].transactions?.length,
+                  itemBuilder: (context, index) {
+                    final transaction = state.accounts[0].transactions?[index];
+                    final balanceInEuros = transaction!.amount / 100;
+                    return ListTile(
+                      title: Text(transaction!.name),
+                      subtitle: Text(DateFormat('dd/MM/yyyy').format(DateTime.parse(transaction!.date))),
+                      trailing: Text("${balanceInEuros.toStringAsFixed(2)}€"),
+                    );
+                  },
+                ),
+              ),
+            ],
             );
           } else if (state is AccountError) {
             return Center(child: Text(state.message));
