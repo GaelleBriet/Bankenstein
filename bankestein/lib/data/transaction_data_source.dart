4bbe45e9a7eaf1f45c39a01e69790b50ce695613
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-04 12:47:24 +0100

    create transaction cubit/state/dataSource

---
+++ bankestein/lib/data/transaction_data_source.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/transaction.dart';
+import '../services/transaction_service.dart';
+
+abstract class TransactionDataSource {
+  static const baseUrl = 'http://192.168.1.32:8000';
+
+  static Future<List<Transaction>> getAccountTransactions(
+      String accessToken, int id) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/me/accounts/$id/transactions'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> responseBody = jsonDecode(response.body);
+      List<Transaction> transactions =
+          responseBody.map((e) => Transaction.fromJson(e)).toList();
+      transactions = TransactionService.sortTransactions(transactions, id);
+      return transactions;
+    } else {
+      throw Exception(
+          'Failed to load transactions with status code: ${response.statusCode} and body: ${response.body}');
+    }
+  }
+}
