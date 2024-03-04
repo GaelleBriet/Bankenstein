563e94a1b92ebc7c88eba7e9bb694544d8d6f2c4
Samantha Uriostegui <79147788+uriostegui-sam@users.noreply.github.com>
2024-03-04 17:19:36 +0100

    cubit and state recipient working

---
+++ bankestein/lib/bloc/recipient_state.dart
@@ -0,0 +1,18 @@
+import 'package:bankestein/models/recipient.dart';
+
+abstract class RecipientState {}
+
+class RecipientInitial extends RecipientState {}
+
+class RecipientLoading extends RecipientState {}
+
+class RecipientLoaded extends RecipientState {
+  final List<Recipient> recipients;
+  RecipientLoaded(this.recipients);
+}
+
+class RecipientError extends RecipientState {
+  final String errorMessage;
+
+  RecipientError(this.errorMessage);
+}
