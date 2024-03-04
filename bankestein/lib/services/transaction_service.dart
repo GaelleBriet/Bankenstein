cfd2837bd078d0853b24789224bf3a6d6183afbc
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-04 11:08:27 +0100

    added service to format transactions (positive or negative)

---
+++ bankestein/lib/services/transaction_service.dart
@@ -0,0 +1,29 @@
+import 'package:bankestein/models/transaction.dart';
+
+abstract class TransactionService {
+  static List<Transaction> sortTransactions(List<Transaction> transactions, int accountId) {
+    return transactions.map((transaction) {
+      if (transaction.toAccountId == accountId) {
+        // Si le compte est le bénéficiaire de la transaction, le montant est positif
+        return Transaction(
+          id: transaction.id,
+          fromAccountId: transaction.fromAccountId,
+          toAccountId: transaction.toAccountId,
+          amount: transaction.amount,
+          date: transaction.date,
+          name: transaction.name,
+        );
+      } else {
+        // Si le compte est le débiteur de la transaction, le montant est négatif
+        return Transaction(
+          id: transaction.id,
+          fromAccountId: transaction.fromAccountId,
+          toAccountId: transaction.toAccountId,
+          amount: -transaction.amount,
+          date: transaction.date,
+          name: transaction.name,
+        );
+      }
+    }).toList();
+  }
+}
