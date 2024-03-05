24a4c9e3a94bf39e3ac883bb36cc36fa2e60863d
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-05 15:01:00 +0100

    added blocProvider & BlocListener for TransactionCubit

--- bankestein/lib/views/accounts_list_view.dart
+++ bankestein/lib/views/accounts_list_view.dart
@@ -55,9 +55,12 @@ class AccountsListView extends StatelessWidget {
                   // color: index.isEven ? Colors.grey[200] : Colors.transparent,
                   color: index.isEven
                       ? Theme.of(context).brightness == Brightness.dark
-                      ? Colors.grey[700] // Couleur plus foncée pour le thème sombre
-                      : Colors.grey[300] // Couleur pour le thème clair
-                      : Theme.of(context).brightness == Brightness.light ? Colors.grey[100] : Colors.grey[500],
+                          ? Colors.grey[
+                              700] // Couleur plus foncée pour le thème sombre
+                          : Colors.grey[300] // Couleur pour le thème clair
+                      : Theme.of(context).brightness == Brightness.light
+                          ? Colors.grey[100]
+                          : Colors.grey[500],
                   child: ListTile(
                     title: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
